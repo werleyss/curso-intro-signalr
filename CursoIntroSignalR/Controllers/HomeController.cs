2a2fe07dca48b7a76d8abde1a0dc1dea96013772
werley <werley_desouzasilva@hotmail.com>
2021-10-23 10:46:32 -0300

    Introdução ASP.NET Core SignalR

---
+++ CursoIntroSignalR/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using CursoIntroSignalR.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoIntroSignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Poll()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# curso-intro-signalr
+<h1 align="center">
+    Introdução ao ASP.NET Core SignalR
+</h1>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-curso">Curso</a>
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="ASP.NET Core SignalR" src=".github/signalr.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [.NET Core 3.1](https://dotnet.microsoft.com/)
+
+## 💻 Projeto
+
+Através desse curso, falar do poderoso SignalR que é um framework de código aberto para aplicações em tempo real que foi desenvolvido pela empresa Microsoft e uma aplicação como exemplo.
+
+## 🔖 Curso
+
+No link abaixo você encontra o curso.
+
+- [Introdução ao ASP.NET Core SignalR](https://www.youtube.com/watch?v=Jwoo5qy8hPM&list=PLfpYVugeLJLY06Q6Z5Y-tz8gOPZ8U4TKq&index=1)
+
+
+---
+
+Werley Silva
